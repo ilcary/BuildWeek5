72838d1f9d22fda524df3775c71745abf1658c4a
ilCary <carisiodigiampietro54@gmail.com>
2022-11-14 15:01:55 +0100

    first commit

---
+++ EPIC_ENERGY_SERVICES/src/main/java/com/ilCary/EPIC_ENERGY_SERVICES/EpicEnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ilCary.EPIC_ENERGY_SERVICES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServicesApplication.class, args);
+	}
+
+}
---
+++ EPIC_ENERGY_SERVICES/src/test/java/com/ilCary/EPIC_ENERGY_SERVICES/EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilCary.EPIC_ENERGY_SERVICES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
